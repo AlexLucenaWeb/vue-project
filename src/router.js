bf0981699c106470428f07802993ab96565cb720
janoweb <janoweb@users.noreply.github.com>
2021-04-16 10:47:01 +0100

    fixing router

--- src/router.js
+++ src/router.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-import { CoachDetails } from './pages/coaches/CoachDetails.vue';
-import { CoachesList } from './pages/coaches/CoachesList.vue';
-import { CoachRegistration } from './pages/coaches/CoachRegistration.vue';
-import { ContactCoach } from './pages/requests/ContactCoach.vue';
-import { RequestsRecived } from './pages/requests/RequestsRecived.vue';
-import { NotFoud } from './pages/NotFoud.vue'
+import CoachDetails from './pages/coaches/CoachDetails.vue';
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestsRecived from './pages/requests/RequestsRecived.vue';
+import NotFoud from './pages/NotFoud.vue';
 
 const router = createRouter({
   history: createWebHistory(),
